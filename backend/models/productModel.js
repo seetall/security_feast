661f97ad978c83a802fb8235e61a5e82059178fd
seetall <2016seetal@gmail.com>
2025-02-04 11:37:21 +0545

    product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+// Product Schema for Gift Cards
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
